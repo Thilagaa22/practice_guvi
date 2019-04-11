10fcd5655461cab95f5a76e840109d9e6b19433d
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 19:34:19 +0530

    Update Hellon.py

--- Hellon.py
+++ Hellon.py
@@ -3,7 +3,7 @@ class Hellon:
         self.n= int(input())
     def prints(self):
         for i in range(0,self.n):
-            print("Hello\n")
+            print("\nHello")
 
 h= Hellon()
 h.prints()
