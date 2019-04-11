874ef1f6ff5cbed67d6296b06b3ffc1e7f3de27f
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 19:50:11 +0530

    Update PowerExp.py

--- PowerExp.py
+++ PowerExp.py
@@ -1,13 +1,13 @@
-class powerexp:
+class powerexpr:
     def __init__(self):
         self.nkarr=input()
         self.nkarr=(self.nkarr).split(" ")
         self.nkarr=[int(i) for i in self.nkarr]
-    def calc(self):
+    def cal(self):
         mul=1
         for i in range(0,self.nkarr[1]):
             mul=mul*self.nkarr[0]
         print(mul)
 
-p= powerexp()
-p.calc()
+p= powerexpr()
+p.cal()
