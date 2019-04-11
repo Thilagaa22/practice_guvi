ac4675ea63399cd72828c1a45cf0a774758fe239
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 19:27:05 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,9 @@
-class Largest:
+class Large:
     def __init__(self):
         self.n=input()
         self.nlist= (self.n).split(" ")
         self.nlist=[int(i) for i in self.nlist]
-    def find(self):
+    def findl(self):
         if(self.nlist[0]>self.nlist[1] and self.nlist[0]>self.nlist[2]):
             print(self.nlist[0])
         elif(self.nlist[1]>self.nlist[0] and self.nlist[1]>self.nlist[2]):
@@ -11,6 +11,6 @@ class Largest:
         else:
             print(self.nlist[2])
 
-l= Largest()
-l.find()
+l= Large()
+l.findl()
         
