7cdaac459b7a29ddb3f15cb9010f8ecfb09ba0a8
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 22:45:46 +0530

    Update CountPrimesInARange.py

--- CountPrimesInARange.py
+++ CountPrimesInARange.py
@@ -1,4 +1,4 @@
-class printodds:
+class printodd:
     def __init__(self):
         self.nq=input()
         self.nq=[int(i) for i in self.nq.split(" ")]
@@ -20,5 +20,5 @@ class printodds:
             i=i+1
         return(count)
 
-p=printodds()
+p=printodd()
 print(p.printing())
