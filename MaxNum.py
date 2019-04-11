3c6346b4648bbaa924299072db7259fd2fe00d83
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 22:28:30 +0530

    Update MaxNum.py

--- MaxNum.py
+++ MaxNum.py
@@ -1,12 +1,12 @@
-class MaxNum:
+class Maxnum:
     def __init__(self):
         self.n=int(input())
         self.narr=input()
         self.narr=[int(i) for i in (self.narr).split(" ")]
 
-    def FindMax(self):
+    def FMax(self):
         (self.narr).sort()
         print((self.narr)[len(self.narr)-1])
 
-m=MaxNum()
-m.FindMax()
+m=Maxnum()
+m.FMax()
