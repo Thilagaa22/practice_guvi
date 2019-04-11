66c49289e85b4157d211e4f9da27823ee7398467
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 21:27:40 +0530

    Update StringDot.py

--- StringDot.py
+++ StringDot.py
@@ -1,12 +1,12 @@
 from string import punctuation as p
-class StringDot:
+class Stringdot:
     def __init__(self):
         self.str=input()
-    def AddDot(self):
+    def addDot(self):
         for i in range(0,len(p)):
             if(p[i]=="."):
                 print(self.str+p[i])
                 break
 
-s=StringDot()
-s.AddDot()
+s=Stringdot()
+s.addDot()
