f4555afaa57e29c6f64ac13b973d5fedfef27b3d
narasimhan3737 <36240321+narasimhan3737@users.noreply.github.com>
2019-04-11 20:03:40 +0530

    Update PrintingOddsInARange.py

--- PrintingOddsInARange.py
+++ PrintingOddsInARange.py
@@ -1,8 +1,8 @@
-class printodds:
+class podd:
     def __init__(self):
         self.nq=input()
         self.nq=[int(i) for i in self.nq.split(" ")]
-    def printing(self):
+    def prints(self):
         lst=[]
         for i in range(self.nq[0]+1,self.nq[1]):
             if(i%2!=0):
@@ -13,5 +13,5 @@ class printodds:
             else:
                 print(lst[i],end=" ")
 
-p=printodds()
-p.printing()
+p=podd()
+p.prints()
